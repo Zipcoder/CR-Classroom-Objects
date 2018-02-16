9dec0df1d83ff63898d60e053d36a041cb0f80b6
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-02-16 17:02:56 -0500

    intellij is being weird

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,5 +1,6 @@
 package io.zipcoder;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Student {
     private String firstName;
@@ -7,38 +8,48 @@ public class Student {
     private ArrayList<Double> examScores;
 
     public Student(String firstName, String lastName, Double[] examScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>();
+        this.examScores.addAll(Arrays.asList(examScores));
 
     }
 
-    public void setFirstName(){
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
 
     }
 
     public String getFirstName(){
-        return null;
+        return this.firstName;
     }
 
-    public void setLastName(){
+    public void setLastName(String lastName){
+        this.lastName = lastName;
 
     }
 
     public String getLastName(){
-        return null;
+        return this.lastName;
     }
 
     public int getNumberOfExamsTaken(){
-        return 0;
+        return examScores.size();
     }
 
     public String getExamScores(){
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i<examScores.size(); i++){
+            builder.append("Exam " + (i+1) + " => " + examScores.get(i) + "\n");
+        }
+        return builder.toString();
     }
 
-    public void addExamScore(){
-
+    public void addExamScore(Double newScore){
+        examScores.add(newScore);
     }
 
-    public void setExamScore(){
+    public void setExamScore(int examIndex, Double newScore){
 
     }
 
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -3,6 +3,8 @@ package io.zipcoder;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
+import java.util.ArrayList;
 
 public class StudentTest {
 
@@ -17,14 +19,115 @@ public class StudentTest {
     }
 
     @Test
-    public void firstNameTest() {
+    public void getFirstNameTest() {
         //Given
+        //Student stinkyPete
 
         //When
-        String expectedFirstName = stinkyPete.getFirstName();
-        String expectedLastName = stinkyPete.getLastName();
+        String expected = "Stinky";
+        String actual = stinkyPete.getFirstName();
 
+        //Then
+        Assert.assertTrue(actual.equals(expected));
+    }
 
+    @Test
+    public void getLastNameTest() {
+        //Given
+        //Student stinkyPete
+
+        //When
+        String expected = "Pete";
+        String actual = stinkyPete.getLastName();
+
+        //Then
+        Assert.assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    public void setFirstNameTest() {
+        //Given
+        stinkyPete.setFirstName("Smelly");
+
+        //When
+        String expected = "Smelly";
+        String actual = stinkyPete.getFirstName();
+
+        //Then
+        Assert.assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    public void setLastNameTest() {
+        //Given
+        stinkyPete.setLastName("Peter");
+
+        //When
+        String expected = "Peter";
+        String actual = stinkyPete.getLastName();
+
+        //Then
+        Assert.assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    public void getNumberOfExamsTakenTest() {
+        //Given
+        //Student stinkyPete
+
+        //When
+        int expected = 3;
+        int actual = stinkyPete.getNumberOfExamsTaken();
+
+        //Then
+        //System.out.println(actual);
+        Assert.assertTrue(actual == expected);
+    }
+
+    @Test
+    public void getExamScoresTest(){
+        //Given
+        //Student stinkyPete
+
+        //When
+        String expected = "Exam 1 => 45.67\n" +
+                "Exam 2 => 92.34\n" +
+                "Exam 3 => 100.0\n";
+        String actual = stinkyPete.getExamScores();
+
+        //Then
+        Assert.assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    public void addExamScoreTest() {
+        //Given
+        stinkyPete.addExamScore(98.45);
+
+        //When
+        String expected = "Exam 1 => 45.67\n" +
+                "Exam 2 => 92.34\n" +
+                "Exam 3 => 100.0\n" +
+                "Exam 4 => 98.45\n";
+        String actual = stinkyPete.getExamScores();
+
+        //Then
+        Assert.assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    public void setExamScoreTest() {
+        //Given
+        stinkyPete.setExamScore(0, 98.45);
+
+        //When
+        String expected = "Exam 1 => 98.45\n" +
+                "Exam 2 => 92.34\n" +
+                "Exam 3 => 100.0\n" +
+        String actual = stinkyPete.getExamScores();
+
+        //Then
+        Assert.assertTrue(actual.equals(expected));
     }
 
 
