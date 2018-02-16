f1de5b745f254f7e547f0bc561f686d6b6c27497
ghost1497 <rfrankie04@gmail.com>
2018-02-16 16:40:53 -0500

    tests created and init fails

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,12 +1,27 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
     private String firstName;
     private String lastName;
-    private int[] testScores;
+    private ArrayList<Double> examScores;
     // These are helper methods to assist us with array operations.
     private int totalExams;
     private int examsTaken;
+
+    /**
+     * How we're going to construct our students when we know how many tests they're going to take
+     * @param firstName
+     * @param lastName
+     * @param examScores
+     */
+    public Student(String firstName, String lastName, Double[] examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores.addAll(Arrays.asList(examScores));
+    }
 
     /**
      * Constructor for a student with just their first and last name.
@@ -15,91 +30,57 @@ public class Student {
      * @param lastName
      */
     public Student(String firstName, String lastName) {
-
-    }
-
-    /**
-     * How we're going to construct our students when we know how many tests they're going to take
-     * @param firstName
-     * @param lastName
-     * @param totalExams
-     */
-    public Student(String firstName, String lastName, int totalExams) {
-
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getFirstName() {
-        return null;
+        return this.firstName;
     }
 
     public void setFirstName(String firstName) {
-
+        this.firstName = firstName;
     }
 
     public String getLastName() {
-        return null;
+        return this.lastName;
     }
 
     public void setLastName(String lastName) {
-
+        this.lastName = lastName;
     }
 
     public int getTotalExams() {
-        return 0;
+        return this.totalExams;
     }
 
     public void setTotalExams(int totalExams) {
-
+        this.totalExams = totalExams;
     }
 
     public int getExamsTaken() {
-        return 0;
+        return this.examsTaken;
     }
 
 
-    /**
-     * What we want here is a string that, if a student hasn't taken any tests, returns a `no tests taken` string.
-     * Otherwise, we want to return something kind of like:
-     * Test Scores:
-     * Test 1 -> 100
-     * Test 2 -> 89
-     * Test 3 -> 54
-     * @return The test scores in a nice string representation.
-     */
-    public String printExamScores() {
+    public String getExamScores(){
         return null;
     }
 
-    /**
-     * This function should add the grade in the correct position in the testScores array.
-     * Make sure the score is between 0 and 100, and that the student isn't taking more tests than they're supposed to.
-     * If there is a problem, print an error message and return false.
-     * Otherwise, return true and put the score in the right spot.
-     * @param score
-     * @return A boolean based on if the operation worked or not.
-     */
-    public boolean takeExam(int score) {
-        return false;
+    public String addExamScore(Double[] examScores){
+        return null;
     }
 
-    /**
-     * Change the score for one of the students tests.
-     * Be aware that the new score must be between 0 and 100, and that they have already taken that test
-     * since it doesn't make sense to change the grade on an exam that they haven't taken it.
-     * @param examNum Which test the we want to change.
-     * @param newScore What we want to change it to.
-     * @return A boolean based on if the operation worked or not.
-     */
-    public boolean changeScoreForExam(int examNum, int newScore){
-        return false;
+    public String setExamScore(int examNumber, double newScore){
+        return null;
     }
 
-    /**
-     * Return the average for all of the exams that the student has taken.
-     * If they haven't taken any, be nice and give them 100.0.
-     * @return The average for all the exams a student has taken.
-     */
-    public double getAverage() {
-        return 100.0;
+    public String getAverageExamScore(){
+        return null;
+    }
+
+    @Override
+    public String toString(){
+        return null;
     }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -4,5 +4,89 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class StudentTest {
+    String firstName = "Frankie";
+    String lastName = "Rodriguez";
 
+
+    @Test
+    public void getExamScoresTestPos(){
+
+        Double[] examScores = { 100.0, 95.0, 123.0, 96.0 };
+        Student student = new Student(firstName, lastName, examScores);
+        String actual = student.getExamScores();
+        Assert.assertTrue(actual.length() > 0);
+    }
+
+    @Test
+    public void getExamScoresTestNeg(){
+
+        Double[] examScores = { 100.0, 95.0, 123.0, 96.0 };
+        Student student = new Student(firstName, lastName, examScores);
+        String actual = student.getExamScores();
+        Assert.assertFalse(actual.equals(""));
+    }
+
+    @Test
+    public void addExamScoreTestPos(){
+        Double[] examScores = { };
+        Student student = new Student(firstName, lastName, examScores);
+        String actual = student.addExamScore(examScores);
+        Assert.assertTrue(actual.length() > 0);
+    }
+
+    @Test
+    public void addExamScoreTestNeg(){
+        Double[] examScores = { };
+        Student student = new Student(firstName, lastName, examScores);
+        String actual = student.addExamScore(examScores);
+        Assert.assertFalse(actual.length() == 0);
+    }
+
+    @Test
+    public void setExamScoreTestPos(){
+        Double[] examScores = { 100.0 };
+        Student student = new Student(firstName, lastName, examScores);
+        String actual = student.setExamScore(1, 150.0);
+        Assert.assertTrue(!actual.equals(examScores));
+    }
+
+    @Test
+    public void setExamScoreTestNeg(){
+        Double[] examScores = { 100.0 };
+        Student student = new Student(firstName, lastName, examScores);
+        String actual = student.setExamScore(1, 150.0);
+        Assert.assertFalse(actual.equals(examScores));
+    }
+
+    @Test
+    public void getAverageExamScoreTestPos(){
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student(firstName, lastName, examScores);
+        String actual = student.getAverageExamScore();
+        Assert.assertTrue(actual.length() > 0);
+    }
+
+    @Test
+    public void getAverageExamScoreTestNeg(){
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student(firstName, lastName, examScores);
+        String actual = student.getAverageExamScore();
+        Assert.assertFalse(actual.length() == 0);
+    }
+
+    @Test
+    public void toStringTestPos(){
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student(firstName, lastName, examScores);
+        String actual = student.toString();
+        Assert.assertTrue(actual.length() > 0);
+    }
+
+    @Test
+    public void toStringTestNeg(){
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student(firstName, lastName, examScores);
+        String actual = student.toString();
+        Assert.assertFalse(actual.length() == 0);
+    }
 }
