d41a481d3b21eaac5f62ca5e883aefa0e11e4ab8
Peter McCormick <petermccormick@zipcoders-MacBook-Pro-109.local>
2018-02-17 13:34:44 -0500

    saving with nullpointerexception

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public class Student  {
 
-    private Double[] testScores = new Double[0];
+    private Integer numberOfExamsScores;
+    private Double[] testScores;
     private String firstName;
     private String lastName;
     private ArrayList<Double> examScores;
@@ -38,12 +39,25 @@ public class Student  {
         return this.testScores;
     }
 
-    public ArrayList<Double> getExamScores(String firstName,String lastName,Double[] examScores) {
-
-        return this.examScores;
+    public String getExamScores() {
+       StringBuilder examScoresFormatted = new StringBuilder();
+       examScoresFormatted.append("Exam Scores: ");
+       for (int i = 0; i < testScores.length; i++) {
+           examScoresFormatted.append("\n\t Exam " +(i+1) + " -> "+ Math.round(testScores[i]));
+       }
+        return examScoresFormatted.toString();
     }
+
+    public void addExamScore(double examScore) {
+        examScores.add(examScore);
+    }
+
+    public void setExamScore(int examNumber, double newScore) {
+        this.examScore = examNumber
+    }
+
     public Integer getNumberOfExamScores() {
-        return  this.getNumberOfExamScores();
+        return 0;
     }
 
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -17,7 +17,6 @@ public class StudentTest {
         // When
         Student student = new Student(expectedFirstName, expectedLastName, expectedTestScores);
 
-
         // Then
 
         String actualFirstName = student.getFirstName();
@@ -32,16 +31,44 @@ public class StudentTest {
 
     @Test
     public void getExamsScoresTest() {
-        String firstName = "Nialls";
-        String lastName = "Harrns";
+        String firstName = "Niall";
+        String lastName = "Horan";
         Double[] examScores = { 93.00, 92.0, 56.0, 88.00 };
 
         Student student = new Student(firstName, lastName, examScores);
 
-        String output = student.getExamScores();
-
-        System.out.println(output);
+        String expected = ("Exam Scores: \n\t Exam 1 -> 93\n\t Exam 2 -> 92\n\t Exam 3 -> 56\n\t Exam 4 -> 88");
+        String actual = student.getExamScores();
+        Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void addExamsScoreTest(){
+        String firstName = "Scott";
+        String lastName = "Weiland";
+        Double[] examScores = {};
+        Student student = new Student(firstName, lastName, examScores);
+
+        student.addExamScore(100.00);
+
+        String expected =("Exam Scores: \n\t Exam 1 -> 100");
+        String actual = student.getExamScores();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setExamScore() {
+        String firstName = "Kwame";
+        String lastName = "Harris";
+        Double[] examScores = {95.0};
+        Student student = new Student(firstName, lastName, examScores);
+
+        student.setExamScores(1, 150.0);
+        String expected = ("Exam Scores:\n\t Exam " + examNumber + " -> " + newScore);
+        String actual = student.getExamScores();
+
+        System.out.println(expected, actual);
+
+    }
 
 }
