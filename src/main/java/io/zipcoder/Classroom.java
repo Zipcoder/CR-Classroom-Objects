2d256ae41c2d5e02bab64a12a713f15cd5d17268
ahsonali <ahsonac@gmail.com>
2018-02-18 12:34:05 -0500

    Not all tests pass, but oh well

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,137 @@
 package io.zipcoder;
 
+import java.util.*;
+import java.lang.StringBuilder;
+
 public class Classroom {
+
+    //Instance Variable
+    private Student[] students;
+
+    //Three Constructors
+    public Classroom() {
+        this(30);
+    }
+
+    public Classroom(int maxNumberOfStudents) {
+        this.students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+
+    public String getStudents() {
+        return Arrays.toString(students);
+    }
+
+    public Double getAverageClassExamScore() {
+        double sumOfAverages = 0;
+        double studentCount = 0;
+
+
+        for (int i = 0; i < this.students.length; i++) {
+            if (students[i] != null) {
+                sumOfAverages += students[i].getAverageExamScore();
+                studentCount++;
+            }
+        }
+        return sumOfAverages/studentCount;
+    }
+
+    public boolean addStudent(String firstName, String lastName, Double[] testScores) {
+        for (int i = 0; i < this.students.length; i++) {
+            if (students[i] == null) {
+                students[i] = new Student(firstName, lastName, testScores);
+                return true;
+
+            }
+        } return false;
+    }
+
+    public boolean removeStudent(String firstName, String lastName)
+    {
+        for (int i = 0; i < this.students.length; i++)
+        {
+            if (students[i].getFirstName().contains(firstName) && students[i].getLastName().contains(lastName))
+            {
+                students[i] = null;
+                return true;
+            }
+        } return false;
+
+
+    }
+
+    public String listStudentByScore()
+    {
+        ArrayList<Student> noEmptySpaces = new ArrayList<>();
+
+        for (int i = 0; i < this.students.length; i++)
+        {
+            if (this.students[i] != null)
+            {
+                noEmptySpaces.add(this.students[i]);
+            }
+        }
+
+        Collections.sort(noEmptySpaces, Student.averageGradeComparison.thenComparing(Student::getLastName));
+        return noEmptySpaces.toString();
+    }
+
+    public ArrayList<Student> getOrderedStudentAsList()
+    {
+        ArrayList<Student> noEmptySpaces = new ArrayList<>();
+
+        for (int i = 0; i< this.students.length; i++)
+        {
+            if (this.students[i] != null)
+            {
+                noEmptySpaces.add(this.students[i]);
+            }
+        }
+
+        Collections.sort(noEmptySpaces, Student.averageGradeComparison.thenComparing(Student::getLastName));
+        return noEmptySpaces;
+
+    }
+
+    public String gradeClass()
+    {
+        ArrayList<Student> OrderOfListOfStudents = this.getOrderedStudentAsList();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < OrderOfListOfStudents.size(); i++)
+        {
+            if((int) Math.ceil(OrderOfListOfStudents.size() * .1) >= i + 1)
+            {
+                sb.append(OrderOfListOfStudents.get(i).getFirstName() + " " + getOrderedStudentAsList().get(i).getLastName() + ": A\n");
+            }
+            else if((int) Math.ceil(OrderOfListOfStudents.size() * .29) >= i + 1)
+            {
+                sb.append(OrderOfListOfStudents.get(i).getFirstName() + " " + getOrderedStudentAsList().get(i).getLastName() + ": B\n");
+            }
+            else if ((int) Math.ceil(OrderOfListOfStudents.size() * .50) >= i + 1)
+            {
+                sb.append(OrderOfListOfStudents.get(i).getFirstName()  + " " + getOrderedStudentAsList().get(i).getLastName() + ": C\n");
+            }
+            else if ((int) Math.ceil(OrderOfListOfStudents.size() * .89) >= i + 1)
+            {
+                sb.append(OrderOfListOfStudents.get(i).getFirstName() + " " + getOrderedStudentAsList().get(i).getLastName() + ": D\n");
+            }
+            else if ((int) Math.ceil(OrderOfListOfStudents.size() * .89) <= i + 1)
+            {
+                sb.append(OrderOfListOfStudents.get(i).getFirstName() + " " + getOrderedStudentAsList().get(i).getLastName() + ": F\n");
+            }
+        } return sb.toString();
+
+    }
+
+
+
+
+
+
+
 }
+
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,93 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
+
+
+
+    @Test
+    public void setFirstNameTest1()
+    {
+        Student student = new Student("James", "Madison");
+        String actual = student.getFirstName();
+        String expected = "James";
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void setFirstNameTest2 ()
+    {
+
+        Double [] testScores = {100.0, 68.0, 87.0, 91.0};
+        Student student = new Student("James", "Madison", testScores);
+        String actual = student.getFirstName();
+        String expected = "James";
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void setLastName1()
+    {
+        Student student = new Student("Mike", "Tyson");
+        String actual = student.getLastName();
+        String expected = "Tyson";
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public  void setLastName2()
+    {
+        Double [] testScores = {87.0, 90.0, 87.0, 78.0, 92.0};
+        Student student = new Student("Mike","Tyson", testScores);
+        String actual = student.getLastName();
+        String expected = "Tyson";
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void setExamScores()
+    {
+        Double [] testScores = {100.0};
+        Student student = new Student("James","Madison", testScores);
+
+        student.setExamScore(1, 94.0);
+        String actual = student.getExamScores();
+        String expected = "Exam Scores:\n\t\tExam 1 -> 100\n\t\tExam 2 -> 94";
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void getAverageExamScoresTest()
+    {
+        Double [] testScores = {86.0, 91.0, 75.0, 98.0};
+        Student student = new Student("James", "Madison", testScores);
+
+        double actual = student.getAverageExamScore();
+        double expected = 87.5;
+
+        Assert.assertEquals(expected, actual, .001);
+    }
+
+    @Test
+    public void overrideToStringTest()
+    {
+        Double [] testScores = {87.0, 98.0, 77.0, 94.0};
+        Student student = new Student("James", "Madison", testScores);
+
+        String expected = "Studnet Name: James Madison\n> Average Score 89\n> Exam Scores:\n\tExam 1 -> 87\n\tExam 2 -> 98\n\tExam 3 -> 77\n\tExam 4 -> 94\n";
+        String actual = student.toString();
+    }
+
+
+
+
+
+
+
+
+
 
 }
