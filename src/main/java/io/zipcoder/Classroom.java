c7435527ee004c5e86f2de8c9c1613f70e59296e
Lawrence Wu <lawrencewu@zipcoders-MacBook-Pro-3.local>
2018-02-18 16:32:12 -0500

    need to make the percentile better but it is okay for now

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,43 +1,31 @@
 package io.zipcoder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class Classroom {
-    private Student [] students;
-    private int studentsEnrolled;
-    private int maxStudentAmount;
+    private Student[] students;
 
-    private HashMap<String, Student> studentHashMap = new HashMap<>();
-
-
-    public Classroom(){
+    public Classroom() {
         this.students = new Student[30];
 
     }
 
-    public Classroom(int maxStudentAmount){
+    public Classroom(int maxStudentAmount) {
         this.students = new Student[maxStudentAmount];
     }
 
-    public Classroom(Student[] students){
+    public Classroom(Student[] students) {
         this.students = students;
     }
 
-    public Student[] getStudents(){
+    public Student[] getStudents() {
         return this.students;
     }
 
-//    public boolean isClassroomFull(){
-//        if (maxStudentAmount > 30){
-//            return false;
-//        }
-//    }
 
-    public boolean addStudents(Student student){
-        for(int i = 0; i < students.length; i++){
-            if (students[i] == (null)){
+    public boolean addStudents(Student student) {
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == (null)) {
                 students[i] = student;
                 return true;
             }
@@ -46,9 +34,9 @@ public class Classroom {
         return false;
     }
 
-    public String removeStudent(String firstName, String lastName){
-        for (int i = 0; i < students.length; i++){
-            if ((students[i].getFirstName().equals(firstName)) && (students[i].getLastName().equals(lastName))){
+    public String removeStudent(String firstName, String lastName) {
+        for (int i = 0; i < students.length; i++) {
+            if ((students[i].getFirstName().equals(firstName)) && (students[i].getLastName().equals(lastName))) {
                 students[i] = null;
                 this.sortNullToEnd();
                 break;
@@ -57,48 +45,86 @@ public class Classroom {
         return "This student is not enrolled";
     }
 
-    public void sortNullToEnd(){
+    public void sortNullToEnd() {
         int counter = 0;
 
         Student[] tempArray = Arrays.copyOf(this.students, this.students.length);
-            for(int i = 0; i < this.students.length; i++){
-                if(!(students[i] == null)){
-                    tempArray[counter] = students[i];
-                    counter++;
-                }
+        for (int i = 0; i < this.students.length; i++) {
+            if (!(students[i] == null)) {
+                tempArray[counter] = students[i];
+                counter++;
             }
-            tempArray[students.length - 1] = null;
-            this.students = tempArray;
+        }
+        tempArray[students.length - 1] = null;
+        this.students = tempArray;
 
     }
 
-    public double getClassAverage(){
+    public double getClassAverage() {
         int sum = 0;
-        for (Student student : this.students){
+        for (Student student : this.students) {
             sum += student.getAverageExamScores();
         }
         return sum / students.length;
 
     }
 
-//    public String getStudentByScore(){
-//
-//    }
 
+    public String getStudentsByScore() {
+        StringBuilder nameAndAverage = new StringBuilder();
 
-    public String getClassScores(){
-        return null;
+        //Cant use an array on collections, gotta use arraylist.
+        ArrayList<Student> studentsAsArrayList = new ArrayList<>();
+        for (int i = 0; i < this.students.length; i++) {
+            if (!(this.students[i] == null)) {
+                studentsAsArrayList.add(this.students[i]);
+            }
+        }
+
+        Collections.sort(studentsAsArrayList);
+        Collections.reverse(studentsAsArrayList);
+
+        for (Student students : studentsAsArrayList) {
+            nameAndAverage.append(students.getFirstName() + " " + students.getLastName() + " " +
+                    ": " + students.getAverageExamScores() + "\n");
+        }
+        return nameAndAverage.toString();
     }
 
-    public void sortStudentsAlphabetically(){
+    public String getGradeBook() {
+        StringBuilder gradeBook = new StringBuilder();
+        ArrayList<Student> sg = new ArrayList<>();
+        for (int i = 0; i < this.students.length; i++) {
+            if (!(this.students[i] == null)) {
+                sg.add(this.students[i]);
 
-    }
 
-    public void sortStudentsByScore(){
+            }
+        }
+        Collections.sort(sg);
+        Collections.reverse(sg);
 
-    }
-
-    public String getGradeBook(){
-        return null;
+        /*
+        10 students
+        10 = F
+        9-7 = D
+        6-4 = C
+        3-2 = B
+        1 = A
+         */
+        for (int i = 0; i < sg.size(); i++) {
+            if ((sg.size() * .10) >= i + 1) {
+                gradeBook.append(sg.get(i).getFirstName() + " " + sg.get(i).getLastName() + " " + sg.get(i).getAverageExamScores() + " : A\n");
+            } else if ((sg.size() * .29) >= i + 1) {
+                gradeBook.append(sg.get(i).getFirstName() + " " + sg.get(i).getLastName() + " " + sg.get(i).getAverageExamScores() + " : B\n");
+            } else if ((sg.size() * .50) >= i + 1) {
+                gradeBook.append(sg.get(i).getFirstName() + " " + sg.get(i).getLastName() + " " + sg.get(i).getAverageExamScores() + " : C\n");
+            } else if ((sg.size() * .89) >= i + 1) {
+                gradeBook.append(sg.get(i).getFirstName() + " " + sg.get(i).getLastName() + " " + sg.get(i).getAverageExamScores() + " : D\n");
+            } else {
+                gradeBook.append(sg.get(i).getFirstName() + " " + sg.get(i).getLastName() + " " + sg.get(i).getAverageExamScores() + " : F\n");
+            }
+        }
+        return gradeBook.toString();
     }
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -2,25 +2,25 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 
-public class Student {
+public class Student implements Comparable <Student> {
     private String firstName;
     private String lastName;
     private ArrayList<Double> testScores;
     private int totalExams;
 
 
-    public Student(String firstName, String lastName, ArrayList<Double> testScores){
+    public Student(String firstName, String lastName, ArrayList<Double> testScores) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.testScores = testScores;
 
     }
 
-    public String getFirstName(){
+    public String getFirstName() {
         return this.firstName;
     }
 
-    public void setFirstName(String firstName){
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
@@ -32,43 +32,43 @@ public class Student {
         this.lastName = lastName;
     }
 
-    public String printTestScores(){
+    public String printTestScores() {
         StringBuilder printTestScores = new StringBuilder();
         printTestScores.append("Exam Scores: \n");
-        for (int i = 0; i < testScores.size(); i++){
-            printTestScores.append("\tExam " + (i+1) + " --> " + testScores.get(i)+ "\n");
+        for (int i = 0; i < testScores.size(); i++) {
+            printTestScores.append("\tExam " + (i + 1) + " --> " + testScores.get(i) + "\n");
         }
         return printTestScores.toString();
     }
 
-    public int getTotalExamsTaken(){
+    public int getTotalExamsTaken() {
         return this.testScores.size();
     }
 
-    public void setTotalExamsTaken(int totalExams){
+    public void setTotalExamsTaken(int totalExams) {
         this.totalExams = totalExams;
     }
 
 
-    public void addExamScore(double examScore){
+    public void addExamScore(double examScore) {
         this.testScores.add(examScore);
     }
 
-    public void setExamScore(int examNum, double newScore){
-        testScores.set(examNum -1, newScore);
+    public void setExamScore(int examNum, double newScore) {
+        testScores.set(examNum - 1, newScore);
     }
 
     public double getAverageExamScores() {
         int sum = 0;
-        for (int i = 0; i <testScores.size(); i++){
+        for (int i = 0; i < testScores.size(); i++) {
             sum += testScores.get(i);
         }
-        double average = (double)sum / testScores.size();
+        double average = (double) sum / testScores.size();
         return Math.round(average);
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder studentBuilder = new StringBuilder("Student Name: ");
         studentBuilder.append(this.firstName + " " + this.lastName);
         studentBuilder.append("\n> Average Score: " + getAverageExamScores() + "\n");
@@ -77,5 +77,19 @@ public class Student {
         return studentBuilder.toString();
     }
 
+
+    private int compareToLastName(Student temp) {
+        return temp.getLastName().compareTo(this.getLastName());
+    }
+
+    @Override
+    public int compareTo(Student temp) {
+        int comparator = Double.compare(this.getAverageExamScores(), temp.getAverageExamScores());
+        if (comparator == 0) {
+            return compareToLastName(temp);
+        }
+        return comparator;
+    }
 }
+
 
