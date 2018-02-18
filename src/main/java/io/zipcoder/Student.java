90121667fe5b29b6fab56be5bb7b4fa291246599
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-02-18 17:20:35 -0500

    not done pushing to meet deadline, still working through

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Formatter;
 
-public class Student {
-
+//Using comparator interface for method in classroom put type being compare in <>
+public class Student implements Comparable<Student> {
 
     private String firstName;
     private String lastName;
     private ArrayList<Double> examScores;
-
 
     public Student(String firstName, String lastName, Double[] testScores) {
         this.firstName = firstName;
@@ -48,12 +47,12 @@ public class Student {
 
 
     public String getExamScores() {
-    //take an array list and transform it into a string
+        //take an array list and transform it into a string
         StringBuilder listScores = new StringBuilder();
         Formatter prettyPrint = new Formatter(listScores);
         // Append all doubles in StringBuilder to the StringBuilder.
-        for(Double studentScore : this.examScores){
-            prettyPrint.format("Exam %d: %.2f%n", (this.examScores.indexOf(studentScore))+1, studentScore);
+        for (Double studentScore : this.examScores) {
+            prettyPrint.format("Exam %d: %.2f%n", (this.examScores.indexOf(studentScore)) + 1, studentScore);
         }
         //trim removes whitespace at end
         return listScores.toString().trim();
@@ -61,34 +60,53 @@ public class Student {
 
     public void addExamScore(double examScore) {
         //add new test score to the list using .add method
-            this.examScores.add(examScore);
+        this.examScores.add(examScore);
 
     }
 
 
     public void setExamScore(int examNum, double updateScore) {
         //updates an existing score using index and .set method
-        this.examScores.set(examNum-1, updateScore);
+        this.examScores.set(examNum - 1, updateScore);
     }
 
 
     public double getAverageExamScore() {
         //Gets average of all exam Scores for a student
         double sum = 0.00;
-        for(Double studentScore : this.examScores){
+        for (Double studentScore : this.examScores) {
             sum += studentScore;
         }
-        return Math.round(sum/this.examScores.size());
+        return Math.round(sum / this.examScores.size());
 
     }
 
     @Override
-  public String toString(){
-         StringBuilder studentRecord = new StringBuilder();
-         Formatter prettyPrint = new Formatter(studentRecord);
-         prettyPrint.format("Student Name: %s %s%nAverage Score: %s%nExam Scores:%n%s%n",
-                 this.getFirstName(), this.getLastName(), this.getAverageExamScore(), this.getExamScores());
+    public String toString() {
+        StringBuilder studentRecord = new StringBuilder();
+        Formatter prettyPrint = new Formatter(studentRecord);
+        prettyPrint.format("Student Name: %s %s%nAverage Score: %s%nExam Scores:%n%s%n",
+                this.getFirstName(), this.getLastName(), this.getAverageExamScore(), this.getExamScores());
         return studentRecord.toString().trim();
-         }
+    }
+
+    /**the function used to sort the collections of students, if we implement comparator Student
+     that means we have  a function that compares one student to another. This function could be in
+     Classroom or student but makes more sense here because its comparing Object of type student
+     Note tried to use comparator but didn't work nicely with sort. Changed to comparable and compareTo
+     sort is working now look into why this is because I don't completely understand*/
+    @Override
+    public int compareTo(Student s) {
+        //-1 means whats on left is less than whats on right(our comparing value)
+        if(this.getAverageExamScore() < s.getAverageExamScore()){
+            return -1;
+        }
+        //1 means whats on left is greater than whats on rigth(our comparing value)
+        if(this.getAverageExamScore() > s.getAverageExamScore()){
+            return 1;
+        }
+        //we cant compare any more cus equal so we let it fall through and do a string comparison
+        return this.getLastName().compareTo(s.getLastName());
+    }
 }
 
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,93 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class ClassroomTest {
+    @Test
+    public void testConstructor_returnsMaxNumStudentsPreDefined() {
+        Classroom testClassRoom = new Classroom();
+        int expected = 30;
+        int actual = testClassRoom.getStudents().length;
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testConstructor_UpdatesMaxNumStudents() {
+        Classroom testClassRoom = new Classroom(20);
+        int expected = 20;
+        int actual = testClassRoom.getStudents().length;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConstructor_EmptyClassroom_ReturnsNothing() {
+        Student[] students = new Student[0];
+        Classroom testClassroom = new Classroom(students);
+        Student[] expected = {};
+        Student[] actual = testClassroom.getStudents();
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testgetAverageExamScore(){
+        //Arrange
+        Double[] sallyScores = { 100.0, 150.0 };
+        Double[] samScores = { 225.0, 25.0 };
+        Student sally = new Student("Sally", "Lapp", sallyScores);
+        Student sam = new Student("Sam", "Emerson", samScores);
+        Student[] students = new Student[]{sally,sam};
+        Classroom classroom = new Classroom(students);
+        //Act
+        double actual = classroom.getAverageExamScore();
+        //Assert
+        Assert.assertTrue(125.00 == actual);
+
+    }
+
+    @Test
+    public void testAddStudent(){
+        //Arrange
+        Double[] billScores = { 225.0, 25.0 };
+        Student bill = new Student("Bill", "DimSum", billScores);
+        Classroom testClassroom = new Classroom(1);
+        Student[] expected = {bill};
+        //Act
+        testClassroom.addStudent(bill);
+        Student[] actual = testClassroom.getStudents();
+        //Assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveStudent(){
+        //Arrange
+        Double[] sallyScores = { 100.0, 150.0 };
+        Double[] samScores = { 225.0, 25.0 };
+        Double[] tomScores = { 100.0, 25.0, 50.00 };
+        Student sally = new Student("Sally", "Lapp", sallyScores);
+        Student sam = new Student("Sam", "Emerson", samScores);
+        Student tom = new Student("Tom", "Lip", tomScores);
+        Student[] students = new Student[]{sally,sam,tom};
+        Classroom classroom = new Classroom(students);
+        //Act
+        classroom.removeStudent("Sally", "Lapp");
+        double expected = 61.0;
+        double actual = classroom.getAverageExamScore();
+        //Assert
+        Assert.assertEquals(expected, actual);
+    }
 }
+
+
+
+
+
+
