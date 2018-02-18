006221b7926ce138e5a49f6f653f8027eeac034e
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-02-18 15:23:06 -0500

    one method left

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -3,46 +3,48 @@ package io.zipcoder;
 import com.sun.xml.internal.fastinfoset.util.StringArray;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Classroom {
 
     private Student[] students;
 
 
-    public Classroom(int maxNumberOfStudents){
+    public Classroom(int maxNumberOfStudents) {
 
         this.students = new Student[maxNumberOfStudents];
     }
 
-    public Classroom(Student[] students){
+    public Classroom(Student[] students) {
 
         this.students = students;
     }
 
-    public Classroom(){
+    public Classroom() {
 
         this.students = new Student[30];
     }
 
-    public Student[] getStudents(){
+    public Student[] getStudents() {
 
         return this.students;
     }
 
-    public double getAverageExamScores(){
+    public double getAverageExamScores() {
 
         double total = 0;
 
-        for (Student student : this.students){
+        for (Student student : this.students) {
             total += student.getAverageExamScore();
         }
 
         return total / students.length;
     }
 
-    public void addStudent(Student student){
+    public void addStudent(Student student) {
 
-        for(int i = 0; i < students.length; i++) {
+        for (int i = 0; i < students.length; i++) {
             if (students[i] == null) {
                 students[i] = student;
                 break;
@@ -50,10 +52,10 @@ public class Classroom {
         }
     }
 
-    public void removeStudent(String firstName, String lastName){
+    public void removeStudent(String firstName, String lastName) {
 
-        for(int i = 0; i < students.length; i++) {
-            if (students[i].getFirstName().equals(firstName) && students[i].getFirstName().equals(firstName)) {
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].getFirstName().equals(firstName) && students[i].getLastName().equals(lastName)) {
                 students[i] = null;
                 this.sortArrayAfterRemoveStudent();
                 break;
@@ -62,27 +64,58 @@ public class Classroom {
     }
 
     // Method called with removeStudentTest //
-    public void sortArrayAfterRemoveStudent(){
+    public void sortArrayAfterRemoveStudent() {
+
         Student[] sortedStudents = Arrays.copyOf(this.students, this.students.length);
 
         int counter = 0;
-        for(int i = 0; i < this.students.length; i++){
-            if(students[i] != null){
+        for (int i = 0; i < this.students.length; i++) {
+            if (students[i] != null) {
                 sortedStudents[counter] = students[i];
                 counter++;
             }
         }
+
+        // Removes copy at end of array //
         sortedStudents[students.length - 1] = null;
+
         this.students = sortedStudents;
     }
 
-    public String getStudentsByScore(){
 
-        return null;
+    public Student[] getStudentsByScore() {
+
+        Arrays.sort(this.students, sortByAverage);
+
+        return this.students;
     }
 
-    public String getGradeBook(){
+    // Will sort students by Average Score //
+    // If two students have the same average then it will sort lexigraphically by Last Name //
+    public static Comparator<Student> sortByAverage = new Comparator<Student>() {
+
+        @Override
+        public int compare(Student student1, Student student2) {
+
+            if (student1.getAverageExamScore() < student2.getAverageExamScore()) {
+                return 1;
+            } else if (student1.getAverageExamScore() > student2.getAverageExamScore()) {
+                return -1;
+            } else {
+                return student1.getLastName().compareToIgnoreCase(student2.getLastName());
+            }
+        }
+    };
+
+    public int getPercentile(){
+
+        return 0;
+    }
+
+    public String getGradeBook() {
+
 
         return null;
     }
 }
+
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -3,7 +3,7 @@ package io.zipcoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Student {
+public class Student{
     private String firstName;
     private String lastName;
     private ArrayList<Double> examScores;
@@ -93,11 +93,5 @@ public class Student {
 
         return combineAll.toString();
     }
-
-
-
-
-
-
 
 }
