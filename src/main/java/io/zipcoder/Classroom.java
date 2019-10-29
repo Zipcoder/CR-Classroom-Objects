261569e446c8beb439ec0222c02e502078640b13
val <valfragier@gmail.com>
2019-10-29 19:54:42 -0400

    AddStudent Classroom Complete

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,41 @@
 package io.zipcoder;
 
 public class Classroom {
+    Student[] students;
+    int MaxNumberOfStudents;
+
+    public Classroom(int MaxNumberOfStudent){
+        MaxNumberOfStudents = MaxNumberOfStudent;
+
+    }
+    public Classroom(Student[] allStudents){
+        this.students = allStudents;
+
+    }
+    public Classroom(){
+        this.students = new Student[30];
+
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Double getAverageExamScore(){
+        Double average = 0.0;
+        for(int i = 0; i < students.length; i++){
+            average += students[i].getAverageExamScore();
+        }
+        return average/students.length;
+    }
+    public void addStudent(Student student){
+        students = new Student[MaxNumberOfStudents];
+        for(int i = 0; i < students.length; i++){
+            if(students[i] == null){
+                students[i] = student;
+                break;
+            }
+        }
+
+    }
 }
