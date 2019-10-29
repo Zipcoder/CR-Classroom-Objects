13cbb98e02e3170cbf7621392f5596110a041546
val <valfragier@gmail.com>
2019-10-29 16:49:54 -0400

    Part 1 Complete

--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,29 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StudentTest {
+    @Test
+    public void getExamScoresTest() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        ArrayList<Double> examScores = new ArrayList<Double>(Arrays.asList(100.0, 95.0, 123.0, 96.0));
+        Student student = new Student(firstName, lastName, examScores);
+        String expected ="Exam Scores:\n" +
+                " \tExam 1 -> 100.0\n" +
+                " \tExam 2 -> 95.0\n" +
+                " \tExam 3 -> 123.0\n" +
+                " \tExam 4 -> 96.0\n";
+        // When
+        String actual = student.getExamScores();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 
 }
