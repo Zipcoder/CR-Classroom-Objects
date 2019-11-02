f847e1516cfcebdd865505c9be115ed5747c1b3f
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-3.local>
2019-11-01 22:17:04 -0400

    Work in progress, need to fix errors

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,50 @@
 package io.zipcoder;
+import java.util.ArrayList;
 
 public class Student {
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, ArrayList<Double>  examScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+
+    }
+
+    public String getExamScores(){
+        String output = "Exam Scores:\n";
+        int count = 1;
+        for(int i = 0; i <= examScores.size()-1; i++){
+            output +=" \tExam " + count + " -> " + examScores.get(i) + "\n";
+            count++;
+
+        }
+        return output;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+
+
 }
