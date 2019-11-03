ef31c769d4d8546c80b4324d8146e75c1531958a
SandyPham823 <sandydupham@gmail.com>
2019-11-03 15:40:33 -0500

    I think I'm getting the hang of tests, maybe

--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,6 +1,14 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
 
+    @Test
+    public void getNumberOfExamsTakenTest(){
+        Student student = new Student("Sandy", "Pham", new Double[] {40.0,50.0});
+        Assert.assertEquals(2, (int)student.getNumberOfExamsTaken());
+    }
 
 }
