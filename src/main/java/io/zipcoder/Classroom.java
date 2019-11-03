d326fc02f9e515df5d4339cbdab7cd310ad62a47
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-11-03 13:52:56 -0500

    completing Classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,11 +1,39 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Classroom {
+    // Declaring variables
+    public int maxNumOfStudents;
+    Student[] students = new Student[30];
+    ArrayList<Student> studentsList = new ArrayList();
 
+    public Classroom(int maxNumOfStudents){
+        this.maxNumOfStudents = maxNumOfStudents;
+    }
 
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
 
+    public Classroom(){
+    }
 
+    public ArrayList<Student> getStudents(){
+        return studentsList;
+    }
 
+    public Double getAverageExamScore() {
+        Double totalExams = 0.0;
+        for (int i = 0; i < students.length; i++) {
+            totalExams += students[i].getAverageExamScore();
+        }
+        return totalExams / students.length;
+    }
+
+    public void addStudent(Student student1) {
+        studentsList.add(student1);
+    }
 
 
 
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,86 @@
 package io.zipcoder;
 
+import com.sun.xml.internal.fastinfoset.tools.XML_SAX_StAX_FI;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ClassroomTest {
+
+
+
+
+
+
+
+
+
+    @Test
+    public void getAverageExamScore() {
+        int maxNumOfStudents = 2;
+        // : Given
+        Double[] s1Scores = {100.0, 150.0};
+        Double[] s2Scores = {225.0, 25.0};
+
+        Student s1 = new Student("student", "one", s1Scores);
+        Student s2 = new Student("student", "two", s2Scores);
+
+        Student[] students = {s1, s2};
+        Classroom classroom = new Classroom(students);
+
+        Double expected = 125.0;
+
+        // When
+        Double output = classroom.getAverageExamScore();
+
+        // Then
+        Assert.assertEquals(expected, output);
+    }
+
+    @Test
+    public void addStudentTest() {
+        // : Given
+        int maxNumberOfStudents = 1;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student s1 = new Student("Leon", "Hunter", examScores);
+
+        String expected = " ===========================\n" +
+                " []\n" +
+                " ===========================\n" +
+                " [Student Name: Leon Hunter\n" +
+                " > Average Score: 125\n" +
+                " > Exam Scores:\n" +
+                "     Exam 1 -> 100\n" +
+                "     Exam 2 -> 150\n" +
+                "     Exam 3 -> 250\n" +
+                "     Exam 4 -> 0";
+
+        // When
+        ArrayList<Student> preEnrollment = classroom.getStudents();
+        classroom.addStudent(s1);
+        ArrayList<Student> postEnrollment = classroom.getStudents();
+
+        // Then
+        String preEnrollmentAsString = preEnrollment.toString();
+        String postEnrollmentAsString = postEnrollment.toString();
+
+        System.out.println("===========================");
+        System.out.println(preEnrollmentAsString);
+        System.out.println("===========================");
+        System.out.println(postEnrollmentAsString);
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
