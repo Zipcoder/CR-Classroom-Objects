1d2e48bf5beb7941a6bfa1c875d95e964e947293
MannyMonocle <eetumadu@gmail.com>
2019-11-04 07:26:17 -0500

    first test works

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,59 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+   String firstName;
+   String  lastName;
+   ArrayList<Double> examScores = new ArrayList<>();
+
+    public Student(String firstName, String lastName, Double[] testScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores.addAll(Arrays.asList(testScores));
+
+    }
+
+    // Getters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public ArrayList<Double> getExamScores() {
+        return examScores;
+    }
+
+    public Integer getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+    // Setters
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // Other Methods
+
+    public String getExamsTaken() {
+        String ans = " Exam Scores:";
+        for (int i = 1; i <= examScores.size(); i++) {
+            ans += "\nExam " + i + " -> " + examScores.get(i);
+        }
+        return ans;
+    }
+
+
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,24 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class StudentTest {
+
+    @Test
+    public void testGetFirstName() {
+        String first = "Lyon";
+        String last = "Hunter";
+        Double[] scores = { 100.0, 95.0, 123.0, 96.0 };
+
+        Student leonHunter = new Student(first, last, scores);
+
+        String actual = leonHunter.getFirstName();
+        System.out.println(actual);
+        Assert.assertEquals("Lyon", actual);
+
+    }
 
 }
