8d6bf0f35f1afa5ab23d03c2e4d3bcf9aef7e2e0
MannyMonocle <eetumadu@gmail.com>
2019-11-04 07:36:55 -0500

    some neg tests added

--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -8,6 +8,19 @@ import java.util.ArrayList;
 public class StudentTest {
 
     @Test
+    public void testGetNotFirstName() {
+        String first = "Lyon";
+        String last = "Hunter";
+        Double[] scores = { 100.0, 95.0, 123.0, 96.0 };
+
+        Student leonHunter = new Student(first, last, scores);
+
+        String actual = leonHunter.getFirstName();
+        Assert.assertNotEquals("first", actual);
+
+    }
+
+    @Test
     public void testGetFirstName() {
         String first = "Lyon";
         String last = "Hunter";
@@ -16,8 +29,37 @@ public class StudentTest {
         Student leonHunter = new Student(first, last, scores);
 
         String actual = leonHunter.getFirstName();
-        System.out.println(actual);
+        //System.out.println(actual);
         Assert.assertEquals("Lyon", actual);
+
+    }
+
+    @Test
+    public void testSetNotFirstName() {
+        String first = "Lyon";
+        String last = "Hunter";
+        Double[] scores = {100.0, 95.0, 123.0, 96.0};
+
+        Student leonHunter = new Student(first, last, scores);
+        leonHunter.setFirstName("Leon");
+
+        String actual = leonHunter.firstName;
+        Assert.assertNotEquals("Lyon", actual);
+
+    }
+
+    @Test
+    public void testSetFirstName() {
+        String first = "Lyon";
+        String last = "Hunter";
+        Double[] scores = { 100.0, 95.0, 123.0, 96.0 };
+
+        Student leonHunter = new Student(first, last, scores);
+        leonHunter.setFirstName("Leon");
+
+        String actual = leonHunter.firstName;
+        //System.out.println(actual);
+        Assert.assertEquals("Leon", actual);
 
     }
 
