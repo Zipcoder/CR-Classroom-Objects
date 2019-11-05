323de54f4975a0ccdb5765a36604d20d80ee405b
MannyMonocle <eetumadu@gmail.com>
2019-11-04 21:18:04 -0500

    classroom constructors made

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,21 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Classroom {
+    ArrayList<Student> students;
+
+    public Classroom(int maxNumberOfStudents){
+        this.students = new ArrayList<>(maxNumberOfStudents);
+    }
+
+   public Classroom(Student[] classroom){
+        this.students.addAll(Arrays.asList(classroom));
+   }
+
+    public Classroom(){
+        this.students = new ArrayList<>(30);
+    }
+
 }
