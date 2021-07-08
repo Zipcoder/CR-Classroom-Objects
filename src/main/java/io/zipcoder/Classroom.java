a7a896986e014f1965405ce14bda83f5123cd091
sitarameez <sitarablossom@gmail.com>
2021-07-07 22:07:38 -0400

    finished student class

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
 public class Classroom {
+private Student[] students;
 
+public Classroom(Student)
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,18 +1,22 @@
 package io.zipcoder;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
-public class Student {
+public class Student extends Object{
     private String firstName;
     private String lastName;
-    private ArrayList<Double> examScores;
+    private ArrayList<Double> examScores = new ArrayList<>();
     private Integer examsTaken;
 
 
     public Student(String firstname, String lastName, Double[] testScores) {
         this.firstName = firstname;
         this.lastName = lastName;
-        this.examScores = new ArrayList<>();
+        for (int i =0; i <testScores.length; i++) {
+            examScores.add(testScores[i]);
+        }
 
     }
 
@@ -39,10 +43,40 @@ public class Student {
     public String getExamScores(){
         StringBuilder score= new StringBuilder();
         for (int i = 0; i < examScores.size(); i++) {
-            score.append("Exam "+ i+1+ " -> " +examScores.get(i));
+            score.append(" Exam "+ (i+1) + " -> " +examScores.get(i));
+            score.append("\n");
         }
         return score.toString();
 
+    }
+
+    public String addExamScore(double examScore){
+        examScores.add(examScore);
+
+        return getExamScores().toString();
+    }
+
+    public String setExamScore(int examNumber, double newScore){
+        examScores.set(examNumber,newScore);
+        return getExamScores().toString();
+    }
+    public Double getAverageExamScore() {
+
+        Double total =0.00;
+        for (int i =0; i < examScores.size(); i++) {
+            total += examScores.get(i);
+
+        } return total / examScores.size();
+    }
+    @Override
+    public String toString(){
+        NumberFormat nf = new DecimalFormat("##");
+        String getExamScores = getExamScores().replace(".0","");
+        String result= "Student Name: "+firstName+" "+lastName+"\n"+"> "+"Average Score: "+
+                Math.round(getAverageExamScore())+"\n" +"> "+ "Exam Scores:" +"\n" +
+                getExamScores;
+
+            return result;
     }
 
 
