b1cd0b330ed9ad8ce310d8c09f02977057cfee6f
Zachary Kitto <zmkitto11@gmail.com>
2021-07-11 14:29:32 -0400

    Added everything except unit tests

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,92 @@
 package io.zipcoder;
 
+import java.util.*;
+
 public class Classroom {
+
+    Student[] students;
+
+    public Classroom(int maxNumberOfStudents) {
+        students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] student) {
+        students = student;
+    }
+
+    public Classroom() {
+        students = new Student[30];
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Double getAverageExamScore() {
+        Double sumOfExamScores = 0.0;
+        for (Student student : students) {
+            sumOfExamScores += student.getAverageExamScore();
+
+        }
+        return sumOfExamScores / students.length;
+    }
+
+    public void addStudent(Student student) {
+        List<Student> arrayList = new ArrayList<>(Arrays.asList(students));
+        arrayList.add(student);
+        students = arrayList.toArray(new Student[arrayList.size()]);
+    }
+
+    public Student[] removeStudent(String firstName, String lastName) {
+        List<Student> arrayList = new ArrayList<>(Arrays.asList(students));
+        for (Student student : students) {
+            if (student.getFirstName().equals(firstName) && student.getLastName().equals(lastName)) {
+                arrayList.remove(student);
+            }
+        }
+        return arrayList.toArray(new Student[arrayList.size()]);
+    }
+
+    public Student[] getStudentByScore() {
+        Arrays.sort(students);
+        return students;
+    }
+
+    public Map getGradeBook() {
+        Map<Student, Character> map = new LinkedHashMap<>();
+        Integer studentsInClass = students.length;
+        getStudentByScore();
+        for (int i = 0; i < studentsInClass; i++) {
+            if ((((i + 1) / studentsInClass) * 100) <= 10) {
+                map.put(students[i], 'A');
+            }
+            else if ((((i + 1) / studentsInClass) * 100) >= 11 &&
+                    (((i + 1) / studentsInClass) * 100) <= 29) {
+                map.put(students[i], 'B');
+            }
+            else if ((((i + 1) / studentsInClass) * 100) >= 30 &&
+                    (((i + 1) / studentsInClass) * 100) <= 50) {
+                map.put(students[i], 'C');
+            }
+            else if ((((i + 1) / studentsInClass) * 100) >= 51 &&
+                    (((i + 1) / studentsInClass) * 100) <= 89) {
+                map.put(students[i], 'D');
+            }
+            else {
+                map.put(students[i], 'F');
+            }
+        }
+        return map;
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Zach", "Kitto", new Double[]{100.0});
+        Student student2 = new Student("Leon", "Hunter", new Double[]{90.0});
+        Student student3 = new Student("Nick", "Wolak", new Double[]{80.0});
+        Student student4 = new Student("Zach", "Singer", new Double[]{70.0});
+        Student student5 = new Student("Jeremy", "Sabina", new Double[]{60.0});
+        Student[] array = new Student[]{student1, student2, student3, student4, student5};
+        Classroom classroom = new Classroom(array);
+        System.out.println(classroom.getGradeBook());
+    }
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,87 @@
 package io.zipcoder;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Student implements Comparable<Student>{
+
+    String firstName;
+    String lastName;
+    List<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        examScores = new ArrayList<Double>(Arrays.asList(testScores));
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getExamScores() {
+        Integer count = 1;
+        StringBuilder builder = new StringBuilder();
+        builder.append("> Exam Scores:\n");
+        Iterator<Double> iterator = examScores.iterator();
+        while (iterator.hasNext()) {
+            builder.append("\tExam ").append(count).append(" -> ").append(iterator.next()).append("\n");
+            count++;
+        }
+        return builder.toString();
+    }
+
+    public Integer getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+    public void addExamScore(double examScore) {
+        examScores.add(examScore);
+    }
+
+    public void setExamScore(int examNumber, double newScore) {
+        examScores.set(examNumber, newScore);
+    }
+
+    public Double getAverageExamScore() {
+        Double sum = 0.0;
+        Iterator iterator = examScores.iterator();
+        while (iterator.hasNext()) {
+            sum += (Double)iterator.next();
+        }
+        return sum / examScores.size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+            builder.append("Student Name: ").append(firstName).append(" ").append(lastName).append("\n")
+                    .append("> Average Score: ").append(getAverageExamScore()).append("\n")
+                    .append(getExamScores());
+            return builder.toString();
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        if (Double.compare(getAverageExamScore(), student.getAverageExamScore()) != 0) {
+            return Double.compare(student.getAverageExamScore(), getAverageExamScore());
+        }
+        else {
+            return this.lastName.compareTo(student.getLastName());
+        }
+    }
 }
